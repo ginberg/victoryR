d34222547d028d19c3c8665fb3e74bd3752abe7e
ginberg <ginberg@gmail.com>
2020-11-02 20:50:56 +0100

    update app

--- app.R
+++ app.R
@@ -2,14 +2,17 @@ library(shiny)
 library(victoryR)
 
 ui <- fluidPage(
-  titlePanel("Victory Example"),
-  sliderInput("n", label = "Number of samples", min = 2, max = 1000, value = 100),
+  titlePanel("Victory Bar chart Example"),
+  sliderInput("n", label = "Number of samples", min = 2, max = 10, value = 5),
   victoryOutput('widgetOutput')
 )
 
 server <- function(input, output, session) {
-  output$widgetOutput <- renderVictory(
-    victory(rnorm(input$n))
-  )
+  output$widgetOutput <- renderVictory({
+    n_samples <- input$n
+    data <- data.frame(x = seq(n_samples), y = 100*seq(n_samples), stringsAsFactors = F)
+    victory(data, "x", "y", type = "Bar")
+  })
 }
+
 shinyApp(ui, server)
